2dc808deaa4e92ef1783d4892ebc4fc25d83b174
anton-b117 <anton@maroonspear.com>
2015-06-06 17:13:17 -0400

    Adding login status with controller using .

---
+++ builds/development/js/controllers/status.js
@@ -0,0 +1,7 @@
+myApp.controller('StatusController', funtion(
+	$scope, $rootScope, $firebaseSimpleLogin) {
+
+	$rootScope.$on('firebaseSimpleLogin:login', function(e, authUser) {
+
+	}); // $firebaseSimpleLoginControllerLogin:Login
+}); // StatusController
